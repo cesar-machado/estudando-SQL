4ec10a2e422bc26349d5ebc20d26cd8464022713
cesar-machado <cesarmach19@gmail.com>
2020-04-20 16:00:37 -0300

    Aprendendo SQL

---
+++ inserirEstados1.sql
@@ -0,0 +1,12 @@
+
+-- Exemplo de Insert 1
+INSERT INTO estados (nome, sigla, regiao, população)
+    VALUES ('Acre', 'AC', 'Norte', 0.83);
+
+-- Exemplo de Insert 2
+INSERT INTO estados 
+    (nome, sigla, regiao, população)
+VALUES
+    ('Alagoas', 'AL', 'Nordeste', 3.38),
+    ('Amapá', 'AP', 'Norte', 0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
